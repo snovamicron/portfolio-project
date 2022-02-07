d35bcf11475c9165c63475b88ccfb72a8e069dc3
snovamicron <mdip5675@gmail.com>
2022-02-07 16:58:46 +0530

    last commit to this project

--- easyChat-client/src/components/menu/Conversation.jsx
+++ easyChat-client/src/components/menu/Conversation.jsx
@@ -1,10 +1,9 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { Grid } from 'semantic-ui-react'
 import { AccountContext } from '../../context/AccountProvider.jsx'
-import { fatchUserData } from '../../services/api'
 import { UserContext } from '../../context/UserProvider'
+import { fatchUserData, getConversation } from '../../services/api'
 import { setConversation } from '../../services/api'
-import { getConversation } from '../../services/api'
 import { ConversationContext } from '../../context/ConversationProvider'
 
 
@@ -13,7 +12,6 @@ const Conversation = ({ text }) => {
     const { userData, setUserData } = useContext(UserContext)
     const { setRoomChat } = useContext(ConversationContext)
     const { googleId } = accountData
-    const [lastMessage, setLastMessage] = useState([])
     const getData = async () => {
         const data = await fatchUserData(googleId)
         setUserData(data)
@@ -24,34 +22,22 @@ const Conversation = ({ text }) => {
         const response = await setConversation(conversationInfo)
         console.log(response);
     }
-    const getLastMessage = async (data)=>{
-        const res = await getConversation(data)
-        setLastMessage([...lastMessage, res])
-       
-    }
-    const showLastMessage = ()=>{
-        console.log(lastMessage);
-        return 'last message'
-    }
     useEffect(() => {
         getData()
         // eslint-disable-next-line
     }, [])
     useEffect(() => {
-      socket.current.on('getUsers', users => {
-        setActiveUsers(users)
-      })
-    });
+        socket.current.on('getUsers', users => {
+            setActiveUsers(users)
+        })
+    }, [])
     
+
     return (
         <>
             <Grid className='conversation' columns={1}>
                 {userData.filter(ele => ele.name.toLowerCase().includes(text.toLowerCase())).map((ele) => {
                     if (ele.googleId !== googleId) {
-                        getLastMessage({
-                            senderId: googleId,
-                            receiverId: ele.googleId
-                        })
                         return (
                             <Grid.Column
                                 className='conversationList'
@@ -69,10 +55,8 @@ const Conversation = ({ text }) => {
                                 <div>
                                     <img referrerPolicy='no-referrer' src={ele.imageUrl} alt="" />
                                     <div>
-                                    <p className='name'>{ele.name}</p>
-                                    <p className="lastMessage">{
-                                        showLastMessage()
-                                    }</p>
+                                        <p className='name'>{ele.name}</p>
+                                        <p className="lastMessage">last message</p>
                                     </div>
                                 </div>
                             </Grid.Column>
