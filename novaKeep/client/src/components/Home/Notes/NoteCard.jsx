9bb71c1f2d16e0fe13d95a7eb2c2fef33031ca9e
snovamicron <mdip5675@gmail.com>
2022-03-03 10:17:18 +0530

    commit-8 on novaKeep date(03/02/2022)

--- novaKeep/client/src/components/Home/Notes/NoteCard.jsx
+++ novaKeep/client/src/components/Home/Notes/NoteCard.jsx
@@ -25,12 +25,18 @@ const useStyles =   makeStyles({
 
 const NoteCard = ({ note })=>{
     const classes = useStyles()
-    const { archive, setArchive, deleted, setDeleted } = useContext(DataContext)
+    const { setArchive, setDeleted, notes, addNotes } = useContext(DataContext)
     const onArchive = ()=>{
-
+       setArchive( oldArr => [...oldArr, notes.find(ele => ele.id === note.id)])
+        let updatedNotes = notes
+        updatedNotes.splice(notes.findIndex(ele => ele.id === note.id), 1)
+        addNotes(updatedNotes)
     }
     const onDelete = ()=>{
-
+        setDeleted( oldArr => [...oldArr, notes.find(ele => ele.id === note.id)])
+        let updatedNotes = notes
+        updatedNotes.splice(notes.findIndex(ele => ele.id === note.id), 1)
+        addNotes(updatedNotes)
     }
     return(
         <>
