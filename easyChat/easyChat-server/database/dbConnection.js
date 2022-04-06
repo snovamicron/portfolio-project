9aa8a6f2d877cf605f99ef92f62b95acd0321244
snovamicron <mdip5675@gmail.com>
2022-04-06 22:33:40 +0530

    modifyed

--- easyChat/easyChat-server/database/dbConnection.js
+++ easyChat/easyChat-server/database/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 
 const connectToMongo = (password) => {
-    const mongodbURI = `mongodb+srv://sonova:${password}@protfolioprojectsdata.f2gzb.mongodb.net/easyChat?retryWrites=true&w=majority`
+    const mongodbURI = `mongodb+srv://soumya:${password}@chat.tfyph.mongodb.net/REAL_TIME_CHAT_APPLICATION?retryWrites=true&w=majority`
     try {
         mongoose.connect(mongodbURI,()=>{
             console.log(`connect to database successfully`);
